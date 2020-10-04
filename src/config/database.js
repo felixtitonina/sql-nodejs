5e03162fe60ff20a5639654f131117253c1abe59
felixtitonina <felixtitonina@outlook.com>
2020-10-04 02:05:58 -0300

    Criando config de banco

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+/**
+ * credenciais da conexao
+ */
+
+module.exports = {
+    dealect: 'postgres',
+    host: 'localhost',
+    username: 'server-postgres',
+    password: '54506083',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true
+    },
+}
